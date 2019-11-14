1645bbf44660b83095fa3dee465ecf357bbea99e
LaybeR <lennard.rizkallah@gmail.com>
2019-11-14 19:57:58 +0100

    Also added some Code 	modified:   src/Industry.java 	modified:   src/MainClass.java

--- src/Industry.java
+++ src/Industry.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Industry {
+
+    public static class SingleIndustry {
+        List<Integer> position;
+        List<Double> usage;
+        int ownPosition;
+        int needed = 0;
+        boolean ownOutput = false;
+
+        public SingleIndustry(Integer ownPosition, List<Double> usage, int needed) {
+            List<Integer> positions = new ArrayList<>();
+            List<Double> usages = new ArrayList<>();
+
+            for (int i = 0; i < usage.size(); i++) {
+                if (usage.get(i) != 0) {
+                    usages.add(usage.get(i));
+                    positions.add(i);
+                }
+            }
+
+            this.ownPosition = ownPosition;
+            this.position = positions;
+            this.usage = usages;
+            if (needed != 0) {
+                this.needed = needed;
+                ownOutput = true;
+            }
+        }
+
+        public boolean isOwnOutput() {
+            return ownOutput;
+        }
+
+        public int getOwnPosition() {
+            return ownPosition;
+        }
+
+        public int getNeeded() {
+            return needed;
+        }
+
+        public List<Double> getUsage() {
+            return usage;
+        }
+
+        public List<Integer> getPosition() {
+            return position;
+        }
+    }
+
+    public boolean checkIfIndustryIsFinal (int position, List<SingleIndustry> singleIndustries) {
+        for (SingleIndustry singleIndustry : singleIndustries) {
+            List<Integer> integers = singleIndustry.getPosition();
+            for (Integer specificPosition : integers) {
+                if (specificPosition == position) return false;
+            }
+        }
+        return true;
+    }
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,14 @@
 import java.io.File;
 import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainClass {
+
+    public static int industries = 20;
+    public static List<Industry.SingleIndustry> singleIndustries;
+
     public static void main(String[] args) {
         MainClass();
     }
@@ -10,11 +16,17 @@ public class MainClass {
     private static void MainClass() {
         //System.out.println(Paths.get("wirtschaft.db").toAbsolutePath().toString());
         try {
-            initDatabase();
+            //initDatabase();
+            singleIndustries = allIndustries();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        if (singleIndustries != null) {
+            Industry.SingleIndustry singleIndustry = singleIndustries.get(0);
+            System.out.println("Industry " + singleIndustry.getOwnPosition() + ":");
+            System.out.println(singleIndustry.getPosition());
+            System.out.println(singleIndustry.getUsage());
+        }
     }
 
     private static void initDatabase() throws Exception{
@@ -22,40 +34,79 @@ public class MainClass {
         boolean firstStart = !testForDatabase();
         Connection conn = DriverManager.getConnection("jdbc:sqlite:wirtschaft.db");
         Statement stat = conn.createStatement();
-        if (firstStart) {
+        if (!firstStart) {
             System.out.println("init");
-            stat.executeUpdate("drop table if exists people;");
-            stat.executeUpdate("create table people (name, occupation);");
+            stat.executeUpdate("drop table if exists industries;");
+            String stringIndustries = "create table industries (";
+            for (int i = 0; i < industries; i++) {
+                stringIndustries = stringIndustries + "I" + i + " STRING DEFAULT 0, ";
+            }
+            stringIndustries = stringIndustries + "I" + industries + " STRING DEFAULT 0);";
+            stat.executeUpdate(stringIndustries);
         }
-        PreparedStatement prep = conn.prepareStatement(
-                "insert into people values (?, ?);");
+        System.out.println(generatePosition() + "|" + generateUsage());
 
-        prep.setString(1, "Gandhi");
-        prep.setString(2, "politics");
-        prep.addBatch();
-        prep.setString(1, "Turing");
-        prep.setString(2, "computers");
-        prep.addBatch();
-        prep.setString(1, "Wittgenstein");
-        prep.setString(2, "smartypants");
-        prep.addBatch();
+        for (int i = 0; i <= industries; i++) {
+            List<String> strings = generateAllPositions();
+            String string = "insert into industries (I" + strings.get(0) + ", I" + strings.get(1) + ", I" + strings.get(2) + ") values ("+ generateUsage() + ", "+ generateUsage() + ", "+ generateUsage() + ");";
+            stat.executeUpdate(string);
+        }
 
-        conn.setAutoCommit(false);
-        prep.executeBatch();
-        conn.setAutoCommit(true);
+        conn.close();
+    }
 
-        ResultSet rs = stat.executeQuery("select * from people;");
+    private static List<Industry.SingleIndustry> allIndustries () throws Exception{
+        List<Industry.SingleIndustry> singleIndustries = new ArrayList<>();
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:wirtschaft.db");
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select * from industries;");
+        Integer industry = 0;
         while (rs.next()) {
-            System.out.println("name = " + rs.getString("name"));
-            System.out.println("job = " + rs.getString("occupation"));
+            List<String> strings = new ArrayList<>();
+            for (int i = 0; i <= industries; i++) {
+                strings.add(rs.getString("I"+i));
+            }
+            List<Double> doubles = new ArrayList<>();
+            for (String string : strings) doubles.add(Double.parseDouble(string));
+            Industry.SingleIndustry singleIndustry = new Industry.SingleIndustry(industry++,doubles, 100);
+            singleIndustries.add(singleIndustry);
         }
         rs.close();
         conn.close();
+        return singleIndustries;
     }
 
     private static boolean testForDatabase() {
         File DBFileTest = new File(Paths.get("wirtschaft.db").toAbsolutePath().toString());
         if (DBFileTest.exists()) return true;
         return false;
+    }
+
+    private static String generateUsage() {
+        double x = Math.random();
+        int i = (int) (x*100);
+        x = ((double) i)/100;
+        return "" + x;
+    }
+
+    private static List<String> generateAllPositions() {
+        List<String> strings = new ArrayList<>();
+        strings.add(generatePosition());
+        strings.add(generatePosition());
+        while (strings.get(0).equals(strings.get(1))) {
+            strings.set(1, generatePosition());
+        }
+        strings.add(generatePosition());
+        while (strings.get(0).equals(strings.get(2)) || strings.get(1).equals(strings.get(2))) {
+            strings.set(2, generatePosition());
+        }
+        return strings;
+    }
+
+    private static String generatePosition () {
+        double x = Math.random();
+        int i = (int) (industries*x) + 1;
+        return "" + i;
     }
 }
